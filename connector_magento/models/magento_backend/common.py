d9f2f1b7a8b4975a17cb32ef156be3b8ca00de29
Marc <mpoch@planetatic.com>
2023-07-03 11:24:10 +0200

    [IMP] connector_magento: black, isort, prettier

--- connector_magento/models/magento_backend/common.py
+++ connector_magento/models/magento_backend/common.py
@@ -36,9 +36,13 @@ class MagentoBackend(models.Model):
 
     @api.model
     def _get_stock_field_id(self):
-        field = self.env["ir.model.fields"].sudo().search(
-            [("model", "=", "product.product"), ("name", "=", "virtual_available")],
-            limit=1,
+        field = (
+            self.env["ir.model.fields"]
+            .sudo()
+            .search(
+                [("model", "=", "product.product"), ("name", "=", "virtual_available")],
+                limit=1,
+            )
         )
         return field
 
